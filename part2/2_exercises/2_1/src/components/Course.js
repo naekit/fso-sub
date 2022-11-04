9eb97260b2ddcb08b347874ec625363d577f59d2
naekit <mykyta.onishchuck@code.berlin>
2022-11-04 17:10:18 +0100

    finished 2_1 through 2_5 with starter code from 1_1 solution

---
+++ part2/2_exercises/2_1/src/components/Course.js
@@ -0,0 +1,26 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part}/>)}
+  </>
+
+const Course = ({course}) => {
+  
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((acc,part) => acc + part.exercises, 0)} />
+    </div>
+  )
+}
+
+export default Course
