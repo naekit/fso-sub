dba0fda04f24736acfbe3b0cf732ffc846e892e3
naekit <mykyta.onishchuck@code.berlin>
2022-11-12 18:04:59 -0500

    added unit tests

---
+++ part4/4_1/utils/list_helper.js
@@ -0,0 +1,56 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((a,b) => a + b.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const fav = blogs.sort((a,b) => b.likes - a.likes)
+    return fav[0]
+}
+
+const mostBlogs = (blogs) => {
+    let count = {}
+    for(let i = 0; i < blogs.length; i++){
+        if(blogs[i].author in count){
+            count[blogs[i].author] += 1
+        } else {
+            count[blogs[i].author] = 0
+        }
+    }
+    console.log(count)
+    const blogger = Object.keys(count).reduce((a,b) => count[a] > count[b] ? a: b)
+    let biggest = blogs.find(({author}) => author === blogger)
+    return {
+        author: biggest.author,
+        blogs: count[biggest.author] + 1
+    }
+}
+
+const mostLikes = (blogs) => {
+    let count = {}
+    for(let i = 0; i < blogs.length; i++){
+        if(blogs[i].author in count){
+            count[blogs[i].author] += blogs[i].likes
+        } else {
+            count[blogs[i].author] = blogs[i].likes
+        }
+    }
+    console.log(count)
+    const blogger = Object.keys(count).reduce((a,b) => count[a] > count[b] ? a: b)
+    let biggest = blogs.find(({author}) => author === blogger)
+    return {
+        author: biggest.author,
+        blogs: count[biggest.author]
+    }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
