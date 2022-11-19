89b1c2a9e2cdb357f3c6d48004c538c5175ac7fc
naekit <mykyta.onishchuck@code.berlin>
2022-11-19 16:30:19 -0500

    part6 redux

---
+++ part6/6_materials/counter/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { createStore } from 'redux'
+
+const noteReducer = (state = [], action) => {
+    if(action.type === 'NEW_NOTE'){
+        return state.concat(action.data)
+    }
+
+    return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'the app state is in redux store',
+        important: true,
+        id: 1
+    }
+})
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2
+    }
+})
+
+const App = () => {
+    return (
+        <div>
+            <h2>notes</h2>
+            <ul>
+                {store.getState().map(note => 
+                    <li key={note.id}>
+                        {note.content} <strong>{note.important ? 'important': ''}</strong>
+                    </li>
+                    )}
+            </ul>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const renderApp = () => root.render(<App />)
+
+renderApp()
+// store.subscribe(renderApp)
