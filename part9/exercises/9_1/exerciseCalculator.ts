2239f543e663af8112d577e73b6edad0887b97b4
naekit <mykyta.onishchuck@code.berlin>
2022-12-06 16:39:55 -0500

    9_6 to 9_7 added linting and post request

--- part9/exercises/9_1/exerciseCalculator.ts
+++ part9/exercises/9_1/exerciseCalculator.ts
@@ -1,11 +1,11 @@
 interface ResultObject {
-    periodLength: Number;
-    trainingDays: Number;
-    success: Boolean;
-    rating: Number;
-    ratingDescription: String;
-    target: Number;
-    average: Number;
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
 }
 
 interface ParamsForCalculator {
@@ -15,31 +15,31 @@ interface ParamsForCalculator {
 
 const parseArguments = (args: Array<string>): ParamsForCalculator => {
     if(args.length < 4) throw new Error(`Not enough arguments`);
-    let exerciseHourDays = []
-    let targetNum =  0
+    const exerciseHourDays = [];
+    let targetNum =  0;
     if(!isNaN(+args[2])){
-        targetNum = +args[2]
+        targetNum = +args[2];
     } else {
-        throw new Error(`Target is not a number, please provide a number target`)
+        throw new Error(`Target is not a number, please provide a number target`);
     }
     for(let i = 3; i < args.length; i++){
         if(!isNaN(+args[i])){
-            exerciseHourDays.push(+args[i])
+            exerciseHourDays.push(+args[i]);
         } else {
-            throw new Error(`Day number ${i - 2} is not a number, please provide hour in number form`)
+            throw new Error(`Day number ${i - 2} is not a number, please provide hour in number form`);
         }
     }
     return {
         numArray: exerciseHourDays,
         target: targetNum
-    }
-}
+    };
+};
 
-const calculateExercises = (days: Array<number>, target: number): ResultObject => {
-    const averageTime = days.reduce((a,c) => a + c, 0) / days.length
+export const calculateExercises = (days: Array<number>, target: number): ResultObject => {
+    const averageTime = days.reduce((a,c) => a + c, 0) / days.length;
     const rating = averageTime < target ? 1
         : averageTime === target ? 2
-        : 3
+        : 3;
     return {
         periodLength: days.length,
         trainingDays: days.filter(x => x !== 0).length,
@@ -48,16 +48,16 @@ const calculateExercises = (days: Array<number>, target: number): ResultObject =
         ratingDescription: rating === 1 ? 'bad, do better': rating === 2 ? 'good job': 'congrats! killed it.',
         target,
         average: averageTime
-    }
-}
+    };
+};
 
 try {
     const { numArray, target } = parseArguments(process.argv);
-    console.log(calculateExercises(numArray, target))
+    console.log(calculateExercises(numArray, target));
 } catch (error: unknown) {
-    let errorMessage = `Something bad happened`
+    let errorMessage = `Something bad happened`;
     if(error instanceof Error) {
-        errorMessage += ` Error: ${error.message}`
+        errorMessage += ` Error: ${error.message}`;
     }
-    console.log(errorMessage)
+    console.log(errorMessage);
 }
--- part9/exercises/9_1/index.ts
+++ part9/exercises/9_1/index.ts
@@ -1,29 +1,50 @@
 import express from 'express';
-import {calculateBmi} from './bmiCalculator';
+import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack');
 });
 
 app.get('/bmi', (req,res) => {
-    const height = Number(req.query.height)
-    const weight = Number(req.query.weight)
-    const bmi = calculateBmi(weight, height)
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    const bmi = calculateBmi(weight, height);
     if(isNaN(height) || isNaN(weight)){
         res.json({
             error: 'malformed parameters'
-        })
+        });
     }
     res.json({
         weight,
         height,
         bmi: bmi
-    })
-})
+    });
+});
+
+app.post('/calculate', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target, } = req.body;
+    console.log(daily_exercises, target);
+
+    if( !daily_exercises || ! target){
+        return res.status(400).send({ error: `parameters missing` });
+    }
+    if(!(daily_exercises instanceof Array) || isNaN(Number(target))){
+        return res.status(400).send({ error: `malformed parameters` });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculateExercises( daily_exercises, target);
+    return res.send(result);
+});
+
 
 const PORT = 3002;
 
 app.listen(PORT, () => {
-    console.log(`listening at http://localhost:${PORT}/bmi`)
-})
+    console.log(`listening at http://localhost:${PORT}`);
+});
