d658f1355dcae2c9c30005411f8fccf8a117ec01
naekit <mykyta.onishchuck@code.berlin>
2022-12-06 12:16:02 -0500

    setting up ts part a & b

---
+++ part9/material/a-b/multiplier.ts
@@ -0,0 +1,44 @@
+interface MultiplyValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): MultiplyValues => {
+    if(args.length < 4) throw new Error(`Not enough arguments`);
+    if(args.length > 4) throw new Error(`Too many arguments`);
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error (`Provided values were not numbers!`)
+    }
+}
+
+const multiplicate = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b)
+}
+
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    multiplicate(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`)
+} catch(error: unknown) {
+    let errorMessage = `Something bad happened`
+    if(error instanceof Error) {
+        errorMessage += ` Error: ${error.message}`
+    }
+    console.log(errorMessage)
+}
+
+
+// const multiplicate = (a: number, b: number, printText: string) => {
+//     console.log(printText, a * b)
+// }
+
+// const a: number = Number(process.argv[2])
+// const b: number = Number(process.argv[3])
+
+// multiplicate( a, b, `multiplied numbers ${a} and ${b}, result is:`)
